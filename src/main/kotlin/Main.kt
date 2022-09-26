8338f8afe1e281fc49837ddd0fa3503219727968
MisterVitoPro <anthony.vito88@gmail.com>
2022-09-26 11:10:19 -0700

    add user input for setting up sim

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,14 @@ import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 
 fun main() {
-    val numOfPlayer = 5
-    val numOfGames = 2000
+    print("Set Number of Players (2-5): ")
+    val numOfPlayer = Integer.valueOf(readLine())
+    println("Number of Players set to: $numOfPlayer")
+
+    print("Set Number of Games to Simulate: ")
+    val numOfGames = Integer.valueOf(readLine())
+    println("Number of Games set to: $numOfGames")
+
     val aggregated = mutableListOf<Results>()
 
     (0 until numOfGames).forEach { _ ->
@@ -16,14 +22,14 @@ fun main() {
     val averageSpaces = aggregated.map { it.playerMoves.average() }.average()
     val playerWins = aggregated.groupingBy { it.winningPlayer!!.id }.eachCount().toSortedMap()
 
-    logger.info { "Games Played: ${aggregated.size}." }
+    logger.info { "Games Played: ${aggregated.size}" }
     for (w in playerWins) {
-        logger.info { "${w.key} (${w.value} wins) - ${aggregated[0].players.first { it.id == w.key }.aiDifficulty}." }
+        logger.info { "${w.key} (${w.value} wins) - ${aggregated[0].players.first { it.id == w.key }.aiDifficulty}" }
     }
-    logger.info { "Average Spaces Moved: ${String.format("%.2f", averageSpaces)}." }
-    logger.info { "Average Number of Turns: ${String.format("%.2f", averageTurns)}." }
-    logger.info { "Shortest Game: ${aggregated.map { it.turns }.minByOrNull { it }}." }
-    logger.info { "Longest Game: ${aggregated.map { it.turns }.maxByOrNull { it }}." }
+    logger.info { "Average Spaces Moved: ${String.format("%.2f", averageSpaces)}" }
+    logger.info { "Average Number of Turns: ${String.format("%.2f", averageTurns)}" }
+    logger.info { "Shortest Game: ${aggregated.map { it.turns }.minByOrNull { it }}" }
+    logger.info { "Longest Game: ${aggregated.map { it.turns }.maxByOrNull { it }}" }
     logger.info { "Average Game Time: ${String.format("%.2f", (averageTurns * 18 / 60))} minutes." }
 }
 
